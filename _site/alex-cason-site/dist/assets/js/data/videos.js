a2bb884825354185c4092fda804c9b4ba37e8998
AJ Cason <ajcason@outlook.com>
2026-08-19 19:36:15 -0400

    M

---
+++ _site/alex-cason-site/dist/assets/js/data/videos.js
@@ -0,0 +1,11 @@
+const CHANNEL_URL = "";
+
+const VIDEOS = [
+  {
+    "title": "Can You Destroy a Universe with a Force?",
+    "description": "A visual, first-principles look at what it would actually take \u2014 and what breaks first \u2014 if you tried to tear a universe apart with a single applied force.\n",
+    "url": "",
+    "thumbnail": "",
+    "date": ""
+  }
+];
